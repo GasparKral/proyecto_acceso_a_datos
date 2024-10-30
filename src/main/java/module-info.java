c72f3aec3d8c2d63372c7ea030d5f4a5694880e3
GasparKral <147488938+GasparKral@users.noreply.github.com>
2024-10-30 16:27:31 +0100

    build(XStream): Add dependecy

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,15 @@
 module es.acceso_a_datos {
     requires javafx.controls;
     requires javafx.fxml;
+    requires xstream;
 
     opens es.acceso_a_datos to javafx.fxml;
     opens es.acceso_a_datos.controllers to javafx.fxml;
     opens es.acceso_a_datos.views to javafx.fxml;
 
+    exports es.acceso_a_datos.controllers;
+    exports es.acceso_a_datos.views;
+    exports es.acceso_a_datos.models;
+    exports es.acceso_a_datos.models.records;
     exports es.acceso_a_datos;
 }
