83b236384e299503965e30b7d3b22c7a65ba3fbc
GasparKral <147488938+GasparKral@users.noreply.github.com>
2024-11-05 19:15:40 +0100

    Debug(XStream lib): solve dependcy problems

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,14 @@
 module es.acceso_a_datos {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
     requires xstream;
 
     opens es.acceso_a_datos to javafx.fxml;
-    opens es.acceso_a_datos.controllers to javafx.fxml;
-    opens es.acceso_a_datos.views to javafx.fxml;
+    opens es.acceso_a_datos.models to javafx.fxml, xstream;
+    opens es.acceso_a_datos.models.records to javafx.fxml, xstream;
+    opens es.acceso_a_datos.controllers to javafx.fxml, xstream;
+    opens es.acceso_a_datos.views to javafx.fxml, xstream;
 
     exports es.acceso_a_datos.controllers;
     exports es.acceso_a_datos.views;
