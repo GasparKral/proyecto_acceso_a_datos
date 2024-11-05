bfe629d68ab2d1a402eb95f1d1451154322c0b99
JuanColi <juancolillabarragan@gmail.com>
2024-11-05 17:39:15 +0100

    refactor (models): add departamento y empleado

--- src/main/java/es/acceso_a_datos/models/Departamento.java
+++ src/main/java/es/acceso_a_datos/models/Departamento.java
@@ -1,5 +1,60 @@
 package es.acceso_a_datos.models;
 
 public class Departamento {
-    
+
+    private int id;
+    private String nombre;
+    private String poblacion;
+    private String Localizacion;
+
+    public Departamento() {
+    }
+
+    public Departamento(int id, String nombre, String poblacion, String localizacion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.poblacion = poblacion;
+        Localizacion = localizacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public String getLocalizacion() {
+        return Localizacion;
+    }
+
+    public void setLocalizacion(String localizacion) {
+        Localizacion = localizacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento:\n" +
+                "id: " + id +
+                "\nnombre='" + nombre +
+                "\npoblacion='" + poblacion+
+                "\nLocalizacion='" + Localizacion;
+    }
 }
--- src/main/java/es/acceso_a_datos/models/Empleado.java
+++ src/main/java/es/acceso_a_datos/models/Empleado.java
@@ -1,5 +1,117 @@
 package es.acceso_a_datos.models;
 
+import java.util.Date;
+
 public class Empleado {
-    
+
+    private int id;
+    private String Nombre;
+    private String Apellido;
+    private int director;
+    private double salario;
+    private String oficio;
+    private Date fecha_alta;
+    private int comision;
+    private Departamento departamento;
+
+    public Empleado() {
+    }
+
+    public Empleado(int id, String nombre, String apellido, int director, double salario, String oficio, Date fecha_alta, int comision, Departamento departamento) {
+        this.id = id;
+        Nombre = nombre;
+        Apellido = apellido;
+        this.director = director;
+        this.salario = salario;
+        this.oficio = oficio;
+        this.fecha_alta = fecha_alta;
+        this.comision = comision;
+        this.departamento = departamento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public int getDirector() {
+        return director;
+    }
+
+    public void setDirector(int director) {
+        this.director = director;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getOficio() {
+        return oficio;
+    }
+
+    public void setOficio(String oficio) {
+        this.oficio = oficio;
+    }
+
+    public Date getFecha_alta() {
+        return fecha_alta;
+    }
+
+    public void setFecha_alta(Date fecha_alta) {
+        this.fecha_alta = fecha_alta;
+    }
+
+    public int getComision() {
+        return comision;
+    }
+
+    public void setComision(int comision) {
+        this.comision = comision;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado:\n" +
+                "id: " + id +
+                "\nNombre: " + Nombre  +
+                "\nApellido: " + Apellido  +
+                "\ndirector: " + director +
+                "\nsalario: " + salario +
+                "\noficio: " + oficio  +
+                "\nfecha_alta: " + fecha_alta +
+                "\ncomision: " + comision +
+                "\ndepartamento: " + departamento;
+    }
 }
