e0379100aac56a2a9b1e07fdc9bf85ff0a88b99a
David <dramirezg03@educarex.es>
2024-11-05 18:58:26 +0100

    refactor(controladorEmpleados): update CRUD method

--- src/main/java/es/acceso_a_datos/controllers/ControladorEmpleados.java
+++ src/main/java/es/acceso_a_datos/controllers/ControladorEmpleados.java
@@ -13,11 +13,11 @@ public class ControladorEmpleados extends HashSet<Empleado> {
         // Implementación futura
     }
 
-    public void modificarEmpleado(int idOriginal,int id, String apellido, int director, double salario, String oficio, Date fecha_alta, Double comision, Integer departamento) {
+    public void modificarEmpleado(int idOriginal, String apellido, int director, double salario, String oficio, Date fecha_alta, Double comision, Integer departamento) {
         Empleado empleadoOriginal = null; // Creamos una objeto empleado para buscar en la coleccion
-        Empleado empleadoReemplazo = new Empleado(id,apellido,director,salario,oficio,fecha_alta,comision,departamento); // Creamos al empleado que reemplazará al empleado original
+        Empleado empleadoReemplazo = new Empleado(idOriginal,apellido,director,salario,oficio,fecha_alta,comision,departamento); // Creamos al empleado que reemplazará al empleado original
         for (Empleado e : this) { // Recorremos todas la colleccion 
-            if (e.getId() == id) { // Comprobamos que el id que nos han pasado coincida con el de algun empleado
+            if (e.getId() == idOriginal) { // Comprobamos que el id que nos han pasado coincida con el de algun empleado
                 empleadoOriginal = e; // En caso de que asi sea asignamos el empleado a nuestro objeto creado anteriormente
                 break;
             }
@@ -29,7 +29,8 @@ public class ControladorEmpleados extends HashSet<Empleado> {
         }
     }
 
-    public void crearEmpleado(int id, String apellido, int director, double salario, String oficio, Date fecha_alta, Double comision, Integer departamento) {
+    public void crearEmpleado(String apellido, int director, double salario, String oficio, Date fecha_alta, Double comision, Integer departamento) {
+        int id = this.size()+1; // 
         Empleado empleado = new Empleado(id,apellido,director,salario,oficio,fecha_alta,comision,departamento); // Creamos el nuevo objeto empleado que se va añadir a la coleccion
         this.add(empleado); //Añadimos a nuestro HashSet el nuevo empleado
     }
