241f2818a077b4fc3599910f93137377ce738a46
JuanColi <juancolillabarragan@gmail.com>
2024-11-06 17:56:37 +0100

    feat(Controllers) Update ControladorDepartamentos

--- src/main/java/es/acceso_a_datos/controllers/ControladorDepartamentos.java
+++ src/main/java/es/acceso_a_datos/controllers/ControladorDepartamentos.java
@@ -1,13 +1,17 @@
 package es.acceso_a_datos.controllers;
+import java.util.Date;
 import java.util.HashSet;
 
 import es.acceso_a_datos.models.Departamento;
+import es.acceso_a_datos.models.Empleado;
 
 public class ControladorDepartamentos {
-
+    
+    private HashSet<Departamento> departamentos = new HashSet<>();
+    
     public void modificarDepartamento(int id, String nombre ,String localizacion){
 
-        HashSet<Departamento> departamentos = new HashSet<>();
+        
         Departamento departamentoInicial = null; // Declaramos un objeto de la clase departamento y lo inicializamos a null para almacenar mas tarde el departamento a modificar
         Departamento departamentoRemplazo = new Departamento(id,nombre,localizacion); //Declaramos otro 
 
@@ -22,4 +26,12 @@ public class ControladorDepartamentos {
         }
 
     }
+
+    public void crearDepartamento(String apellido, int director, double salario, String oficio, Date fecha_alta,
+            Double comision, Integer departamento) {
+        int id = this.departamentos.size() + 1; //
+        Empleado empleado = new Empleado(id, apellido, director, salario, oficio, fecha_alta, comision, departamento); // Creamos el nuevo objeto empleado que se va añadir a la coleccion
+        this.depar.add(empleado); // Añadimos a nuestro HashSet el nuevo empleado
+    }
+
 }
