1b78b753187e04981969807932723abbd7f93ed9
JuanColi <juancolillabarragan@gmail.com>
2024-11-06 14:20:58 +0100

    feat(Controllers) Update ControladorDepartamentos

--- src/main/java/es/acceso_a_datos/controllers/ControladorDepartamentos.java
+++ src/main/java/es/acceso_a_datos/controllers/ControladorDepartamentos.java
@@ -7,21 +7,19 @@ public class ControladorDepartamentos {
 
     public void modificarDepartamento(int id, String nombre ,String localizacion){
 
+        HashSet<Departamento> departamentos = new HashSet<>();
         Departamento departamentoInicial = null; // Declaramos un objeto de la clase departamento y lo inicializamos a null para almacenar mas tarde el departamento a modificar
         Departamento departamentoRemplazo = new Departamento(id,nombre,localizacion); //Declaramos otro 
 
-        for (Departamento dep : this){
+        for (Departamento dep : departamentos){
             if (dep.getId()==id) {
                 departamentoInicial=dep;
             }
         }
         if (departamentoInicial!=null) {
-            this.remove(departamentoInicial);
-            this.add(departamentoRemplazo);
+            departamentos.remove(departamentoInicial);
+            departamentos.add(departamentoRemplazo);
         }
 
     }
-
-    HashSet<Departamento> departamentos = new HashSet<>();
-
 }
