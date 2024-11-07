b2057ef64a320e48036db5645f60095e188910a6
David <dramirezg03@educarex.es>
2024-11-07 13:00:08 +0100

    feat(Controllers) Add new methods

--- src/main/java/es/acceso_a_datos/controllers/ControladorEmpleados.java
+++ src/main/java/es/acceso_a_datos/controllers/ControladorEmpleados.java
@@ -8,29 +8,65 @@ import es.acceso_a_datos.models.Empleado;
 public class ControladorEmpleados {
 
     public HashSet<Empleado> empleados = new HashSet<>();
+    
+    public enum CampoBusqueda { //Utilizo un enum para facilitar la busqueda
+        ID, APELLIDO, DIRECTOR, SALARIO, OFICIO, FECHA_ALTA, COMISION, DEPARTAMENTO
+    }
 
-    public HashSet<Empleado> buscarEmpleado(int id, String apellido, int director, double salario, String oficio,
-    Date fecha_alta, Double comision, Integer departamento){
-        Empleado empleadoABuscar = null; // Creamos una objeto para el empleado a eliminar
+    public HashSet<Empleado> buscarEmpleado(String valor, CampoBusqueda campo) { //Metodo que sirve para buscar un empleado, el cual devuelve un HashSet de empleados
 
-        for (Empleado e : this.empleados) { // Recorremos todas la colleccion
-            if (e.getId() == id) { // Comprobamos que el id que nos han pasado coincida con el de algun empleado
-                empleadoABuscar = e; // En caso de que asi sea asignamos el empleado a nuestro objeto creado anteriormente
-                break;
+        HashSet<Empleado> empleadosARetornar = new HashSet<>(); // Creamos el HashSet que se retornará
+        
+        for (Empleado e : this.empleados) { // Recorremos todos los empleados existentes
+            switch(campo) { // Hacemos un switch para separar segun el campo por el que se busca y segun el que sea comparamos con valor dado
+                case ID:
+                    if (String.valueOf(e.getId()).equals(valor)) {
+                        empleadosARetornar.add(e);
+                    }
+                    break;
+                case APELLIDO:
+                    if (e.getApellido().equals(valor)) {
+                        empleadosARetornar.add(e);
+                    }
+                    break;
+                case DIRECTOR:
+                    if (String.valueOf(e.getDirector()).equals(valor)) {
+                        empleadosARetornar.add(e);
+                    }
+                    break;
+                case SALARIO:
+                    if (String.valueOf(e.getSalario()).equals(valor)) {
+                        empleadosARetornar.add(e);
+                    }
+                    break;
+                case OFICIO:
+                    if (e.getOficio().equals(valor)) {
+                        empleadosARetornar.add(e);
+                    }
+                    break;
+                case FECHA_ALTA:
+                    if (e.getFecha_alta().toString().equals(valor)) {
+                        empleadosARetornar.add(e);
+                    }
+                    break;
+                case COMISION:
+                    if (String.valueOf(e.getComision()).equals(valor)) {
+                        empleadosARetornar.add(e);
+                    }
+                    break;
+                case DEPARTAMENTO:
+                    if (String.valueOf(e.getDepartamento()).equals(valor)) {
+                        empleadosARetornar.add(e);
+                    }
+                    break;
             }
         }
-        if (empleadoABuscar != null) {
-            this.empleados.remove(empleadoAEliminar); // Eliminamos el empleado
-        }
-        if (id != null) {
-
-        }
-
+        
+        return empleadosARetornar; // Retornamos el HashSet
     }
 
 
-    public void modificarEmpleado(int idOriginal, String apellido, int director, double salario, String oficio,
-            Date fecha_alta, Double comision, Integer departamento) {
+    public void modificarEmpleado(int idOriginal, String apellido, int director, double salario, String oficio, Date fecha_alta, Double comision, Integer departamento) {
         Empleado empleadoOriginal = null; // Creamos una objeto empleado para buscar en la coleccion
         Empleado empleadoReemplazo = new Empleado(idOriginal, apellido, director, salario, oficio, fecha_alta, comision,
                 departamento); // Creamos al empleado que reemplazará al empleado original
@@ -47,8 +83,7 @@ public class ControladorEmpleados {
         }
     }
 
-    public void crearEmpleado(String apellido, int director, double salario, String oficio, Date fecha_alta,
-            Double comision, Integer departamento) {
+    public void crearEmpleado(String apellido, int director, double salario, String oficio, Date fecha_alta, Double comision, Integer departamento) {
         int id = this.empleados.size() + 1; //
         Empleado empleado = new Empleado(id, apellido, director, salario, oficio, fecha_alta, comision, departamento);
         // Creamos el nuevo objeto empleado que se va añadir a la coleccion
