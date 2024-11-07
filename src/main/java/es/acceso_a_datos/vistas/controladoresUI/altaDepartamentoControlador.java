e2f8f6bc0bcf1cd160af78f4bab42f573c49a63d
David <dramirezg03@educarex.es>
2024-11-07 21:03:25 +0100

    feat(controllers,fxml)Add atributes

--- src/main/java/es/acceso_a_datos/vistas/controladoresUI/altaDepartamentoControlador.java
+++ src/main/java/es/acceso_a_datos/vistas/controladoresUI/altaDepartamentoControlador.java
@@ -1,8 +1,19 @@
 package es.acceso_a_datos.vistas.controladoresUI;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class altaDepartamentoControlador {
+    @FXML
+    TextField tFLocalizacion;
+    @FXML
+    TextField tFNombre;
+
+    public void initialize(){
+        
+    }
+
 
     public void nuevoDepartamentoOnAction(ActionEvent event) {
 
--- src/main/java/es/acceso_a_datos/vistas/controladoresUI/modificarUsuarioControlador.java
+++ src/main/java/es/acceso_a_datos/vistas/controladoresUI/modificarUsuarioControlador.java
@@ -1,8 +1,11 @@
 package es.acceso_a_datos.vistas.controladoresUI;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 
 public class modificarUsuarioControlador {
+
+    public TextField tFApellido;
 
     public void modificarUsuarioOnAction(ActionEvent event) {
 
