e5ae328b4c3085a968465359a1692564b9c84313
GasparKral <147488938+GasparKral@users.noreply.github.com>
2024-11-07 21:58:09 +0100

    Update(SceneOption): Add new scene options

--- src/main/java/es/acceso_a_datos/modelos/records/SceneOptions.java
+++ src/main/java/es/acceso_a_datos/modelos/records/SceneOptions.java
@@ -1,5 +1,6 @@
 package es.acceso_a_datos.modelos.records;
 
-public record SceneOptions(Pair<Integer, Integer> size, Pair<Integer, Integer> position) {
+public record SceneOptions(Pair<Integer, Integer> tamaño, Pair<Integer, Integer> posicion, Boolean maximizado,
+        Boolean redimensionable) {
 
 }
