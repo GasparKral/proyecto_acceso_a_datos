72564c464cc1c8463325ecbe0d8b959a7a9f7637
David <dramirezg03@educarex.es>
2024-11-07 13:37:08 +0100

    feat(fxml,controllers)Create new controllersUI

---
+++ src/main/java/es/acceso_a_datos/vistas/controladoresUI/altaDepartamentoControlador.java
@@ -0,0 +1,10 @@
+package es.acceso_a_datos.vistas.controladoresUI;
+
+import javafx.event.ActionEvent;
+
+public class altaDepartamentoControlador {
+
+    public void nuevoDepartamentoOnAction(ActionEvent event) {
+
+    }
+}
---
+++ src/main/java/es/acceso_a_datos/vistas/controladoresUI/menuControlador.java
@@ -0,0 +1,30 @@
+package es.acceso_a_datos.vistas.controladoresUI;
+
+import javafx.event.ActionEvent;
+
+public class menuControlador {
+
+    public void bAltaOnAction(ActionEvent event) {
+
+    }
+
+    public void bBajaOnAction(ActionEvent event) {
+
+    }
+
+    public void bModificarOnAction(ActionEvent event) {
+
+    }
+
+    public void bListarOnAction(ActionEvent event) {
+
+    }
+
+    public void bBuscarOnAction(ActionEvent event) {
+
+    }
+
+    public void bSalirOnAction(ActionEvent event) {
+
+    }
+}
