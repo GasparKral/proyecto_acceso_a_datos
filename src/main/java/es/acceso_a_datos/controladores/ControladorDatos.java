8a92936999f84699fb0afab5833ad75c4e7fe2ed
GasparKral <147488938+GasparKral@users.noreply.github.com>
2024-11-09 12:24:06 +0100

    RefactorControladorDatos): change collection for Object

--- src/main/java/es/acceso_a_datos/controladores/ControladorDatos.java
+++ src/main/java/es/acceso_a_datos/controladores/ControladorDatos.java
@@ -1,22 +1,21 @@
 package es.acceso_a_datos.controladores;
 
-import java.util.ArrayList;
+import es.acceso_a_datos.modelos.DatosCacheados;
 
 public class ControladorDatos {
     // Leer y guardar CSV
 
-    public ArrayList<String> datosCsv = new ArrayList<>();
+    DatosCacheados datosCacheados = DatosCacheados.getInstance();
 
+    public void cargarCsv() {
 
-    public void cargarCsv(){
-        
-    }
-    
-    public void leerCsv(){
-        
     }
 
-    public  void guardarCsv(){
+    public void leerCsv() {
+
+    }
+
+    public void guardarCsv() {
 
     }
 }
