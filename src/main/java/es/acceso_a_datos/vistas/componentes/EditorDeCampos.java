8a367494926e23dbf0d11bb6e6ddc364391e72c8
GasparKral <147488938+GasparKral@users.noreply.github.com>
2024-11-10 13:41:36 +0100

    Docs(EditorDeCampos): add coments to class

--- src/main/java/es/acceso_a_datos/vistas/componentes/EditorDeCampos.java
+++ src/main/java/es/acceso_a_datos/vistas/componentes/EditorDeCampos.java
@@ -22,42 +22,68 @@ import javafx.scene.layout.VBox;
 
 public class EditorDeCampos extends HBox {
 
+    /**
+     * Propiedad que indica si el editor est abierto o no.
+     */
     public SimpleBooleanProperty estaAbierto = new SimpleBooleanProperty(false);
+
+    /**
+     * Instancia del controlador principal de la aplicaci n.
+     */
     private ControladorPrincipal instancia = ControladorPrincipal.getInstance();
+
+    /**
+     * Constructor de la clase que crea un nuevo editor de campos para el objeto
+     * determinado.
+     * 
+     * @param elemento el objeto que se va a editar.
+     */
 
     public EditorDeCampos(Object elemento) {
 
         super();
 
+        // Establece el estilo de fondo del editor
         this.styleProperty().set("-fx-background-color: #dadada;");
 
+        // Crea un contenedor vertical (VBox) para los campos y botones
         VBox area = new VBox();
+        // Crea un contenedor horizontal (HBox) para los botones
         HBox areaBotones = new HBox();
+        // Crea un contenedor horizontal (HBox) para los campos
         HBox areaCampos = new HBox();
 
+        // Establece el relleno para los contenedores
         area.setPadding(new Insets(10, 10, 10, 10));
         areaCampos.setPadding(new Insets(10, 10, 10, 10));
 
+        // Crea los botones de guardar y eliminar
         Button guardar = new Button("Guardar");
         Button eliminar = new Button("Eliminar");
 
+        // Establece el estilo de peligro para el botón de eliminar
         eliminar.getStyleClass().add("button-danger");
 
+        // Agrega los contenedores y botones al editor
         this.getChildren().add(area);
         area.getChildren().add(areaBotones);
         area.getChildren().add(areaCampos);
 
+        // Agrega los botones al contenedor de botones
         areaBotones.getChildren().add(guardar);
         areaBotones.getChildren().add(eliminar);
 
+        // Establece el espacio entre los botones
         areaBotones.setSpacing(20);
         areaCampos.setSpacing(20);
 
+        // Oculta los elementos del editor por defecto
         this.getChildren().forEach(c -> {
             c.setManaged(false);
             c.setVisible(false);
         });
 
+        // Agrega un listener para mostrar u ocultar los elementos del editor
         estaAbierto.addListener((obs, oldVal, newVal) -> {
             this.getChildren().forEach(c -> {
                 c.setManaged(newVal);
@@ -65,11 +91,15 @@ public class EditorDeCampos extends HBox {
             });
         });
 
+        // Verifica si el elemento es un Empleado
         if (elemento instanceof Empleado) {
-
+            // Crea un empleado a partir del elemento
             Empleado empleado = (Empleado) elemento;
 
+            // Crea una lista de nodos para los campos del empleado
             List<Node> campos = new ArrayList<>();
+
+            // Agrega los campos del empleado a la lista
             campos.add(new VBox(new Label("Apellido"), new TextField(empleado.getApellido())));
             campos.add(new VBox(new Label("Oficio"), new TextField(empleado.getOficio())));
             campos.add(new VBox(new Label("Salario"), new Spinner<>(0, 100000, empleado.getSalario())));
@@ -79,14 +109,18 @@ public class EditorDeCampos extends HBox {
                     new Spinner<>(0, 100, empleado.getDepartamento() == null ? -1 : empleado.getDepartamento())));
             campos.add(new VBox(new Label("Fecha de alta"), new DatePicker(empleado.getFecha_alta())));
 
+            // Verifica si el empleado tiene un director
             if (empleado.getDirector() != null) {
+                // Agrega el campo del director a la lista
                 campos.add(new VBox(new Label("Director"), new TextField(String.valueOf(empleado.getDirector()))));
             }
 
+            // Agrega los campos a el contenedor de campos
             areaCampos.getChildren().addAll(campos);
 
+            // Agrega un listener para el botón de guardar
             guardar.setOnAction(e -> {
-
+                // Recorre los campos y actualiza los valores del empleado
                 areaCampos.getChildren().forEach(c -> {
                     if (c instanceof VBox) {
                         ((VBox) c).getChildren().forEach(n -> {
@@ -104,7 +138,7 @@ public class EditorDeCampos extends HBox {
                             } else {
                                 valor = "";
                             }
-
+                            // Actualiza los valores del empleado según el campo
                             switch (label) {
                                 case "Apellido":
                                     empleado.setApellido((String) valor);
@@ -128,7 +162,7 @@ public class EditorDeCampos extends HBox {
                         });
                     }
                 });
-
+                // Llama al método para modificar el empleado
                 instancia.controladorEmpleados.modificarEmpleado(
                         empleado.getId(),
                         empleado.getApellido(),
@@ -140,30 +174,41 @@ public class EditorDeCampos extends HBox {
                         empleado.getDepartamento());
 
             });
-
+            // Agrega un listener para el botón de eliminar
             eliminar.setOnAction(e -> {
+                // Crea un diálogo de confirmación para eliminar el empleado
                 Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "¿Desea eliminar el empleado?");
                 alert.showAndWait().ifPresent(
                         buttonType -> {
                             if (buttonType == ButtonType.OK) {
+                                // Llama al método para eliminar el empleado
                                 instancia.controladorEmpleados.eliminarEmpleado(empleado.getId());
 
+                                // Actualiza la lista de empleados
                                 BusquedasControlador.getInstance().actualizarListaEmpleados();
                             }
                         });
             });
 
+            // Verifica si el elemento es un Departamento
         } else if (elemento instanceof Departamento) {
-
+            // Crea un departamento a partir del elemento
             Departamento departamento = (Departamento) elemento;
 
+            // Crea una lista de nodos para los campos del departamento
             List<Node> campos = new ArrayList<>();
+
+            // Agrega los campos del departamento a la lista
             campos.add(new HBox(new Label("Nombre"), new TextField(departamento.getNombre())));
             campos.add(new HBox(new Label("Localizacion"), new TextField(departamento.getLocalizacion())));
 
             areaCampos.getChildren().addAll(campos);
 
+            // Agrega un listener para el botón de guardar
+
             guardar.setOnAction(e -> {
+                // Recorre los campos y actualiza los valores del departamento
+
                 areaCampos.getChildren().forEach(c -> {
                     if (c instanceof HBox) {
                         ((HBox) c).getChildren().forEach(n -> {
@@ -179,6 +224,8 @@ public class EditorDeCampos extends HBox {
                             } else {
                                 texto = "";
                             }
+                            // Actualiza los valores del departamento según el campo
+
                             switch (label) {
                                 case "Nombre":
                                     departamento.setNombre(texto);
@@ -190,6 +237,7 @@ public class EditorDeCampos extends HBox {
                         });
                     }
                 });
+                // Llama al método para modificar el departamento
 
                 instancia.controladorDepartamentos.modificarDepartamento(
                         departamento.getId(),
@@ -197,13 +245,18 @@ public class EditorDeCampos extends HBox {
                         departamento.getLocalizacion());
 
             });
+            // Agrega un listener para el botón de eliminar
 
             eliminar.setOnAction(e -> {
+                // Crea un diálogo de confirmación para eliminar el departamento
+
                 Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "¿Desea eliminar el departamento?");
                 alert.showAndWait().ifPresent(
                         buttonType -> {
                             if (buttonType == ButtonType.OK) {
                                 try {
+                                    // Llama al método para eliminar el departamento
+
                                     instancia.controladorDepartamentos.eliminarDepartamento(departamento.getId());
                                     List<Empleado> empleadosCopia = new ArrayList<>(
                                             instancia.controladorEmpleados.empleados);
@@ -217,6 +270,7 @@ public class EditorDeCampos extends HBox {
                                             }
                                         }
                                     });
+                                    // Actualiza la lista de departamentos
 
                                     BusquedasControlador.getInstance().actualizarListaDepartamentos();
 
